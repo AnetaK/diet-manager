129f0a3ea3f31b044d938dde65ef217abcf0ec5d
AnetaK <karbowska.aneta@gmail.com>
2017-01-03 20:36:34 +0100

    file reader

--- src/main/java/pl/dietmanager/data/MeasurementsReader.java
+++ src/main/java/pl/dietmanager/data/MeasurementsReader.java
@@ -1,4 +1,22 @@
 package pl.dietmanager.data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MeasurementsReader {
+    public List<String> readFromFile(String path){
+        List<String> lines = new ArrayList<String>();
+        String line = "";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null ) {
+                lines.add(line);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return lines;
+    }
 }
--- src/test/java/pl/dietmanager/data/MeasurementsReaderTest.java
+++ src/test/java/pl/dietmanager/data/MeasurementsReaderTest.java
@@ -1,7 +1,23 @@
 package pl.dietmanager.data;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MeasurementsReaderTest {
+
+    @Test
+    public void should_return_6_lines(){
+        MeasurementsReader reader = new MeasurementsReader();
+        List<String> strings = reader.readFromFile("src/test/resources/Measurements.csv");
+
+        assertThat("List has size 5", strings.size(), is(equalTo(6)));
+    }
+
+
 
 }
